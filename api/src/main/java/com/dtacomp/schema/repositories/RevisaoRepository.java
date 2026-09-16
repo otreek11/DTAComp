9671aac9e7c990f30e96ef9a364ce3921b9207d8
João Guilherme <otreek11@gmail.com>
2026-09-15 21:26:41 -0300

    feat: repositorios base

--- api/src/main/java/com/dtacomp/schema/repositories/RevisaoRepository.java
+++ api/src/main/java/com/dtacomp/schema/repositories/RevisaoRepository.java
@@ -1,10 +1,15 @@
 package com.dtacomp.schema.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.dtacomp.schema.classes.Conteudo;
 import com.dtacomp.schema.classes.Revisao;
 
-public interface RevisaoRepository 
-    extends JpaRepository<Revisao, UUID> {}
+public interface RevisaoRepository extends JpaRepository<Revisao, UUID> {
+    List<Revisao> findByRevisor_Id(UUID idRevisor);
+    List<Revisao> findByConteudo_Id(UUID idConteudo);
+    List<Revisao> findByRevisor_IdAndStatus(UUID idRevisor, Conteudo.StatusConteudo status);
+}
