0bcadc36d32e943c9bec69f9867738d242dfdc56
Marcelo Albuqueque Heluy <heluymarcelo@gmail.com>
2026-09-17 15:33:50 -0300

    Testes para Usuario e Conteudo, pdf e texto.

---
+++ api/src/test/java/com/dtacomp/schema/classes/ConteudoTest.java
@@ -0,0 +1,63 @@
+package com.dtacomp.schema.classes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.time.LocalDate;
+import java.util.stream.Stream;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.dtacomp.schema.classes.Conteudo.TipoConteudo;
+import com.dtacomp.schema.classes.Conteudo.StatusConteudo;
+
+
+public class ConteudoTest {
+    @Test 
+    public void deveConstruirConteudoComBuilder() {
+        Conteudo conteudo = Conteudo.builder().titulo("Teste").resumo("resumo").tipo(TipoConteudo.TEXTO).status(StatusConteudo.REVISAO).data(LocalDate.of(2026,9,16)).build();
+        assertEquals("Teste", conteudo.getTitulo());
+        assertEquals("resumo", conteudo.getResumo());
+        assertEquals("texto", conteudo.getTipo().getDescription());
+        assertEquals("revisao", conteudo.getStatus().getDescription());
+        assertEquals(LocalDate.of(2026,9,16), conteudo.getData());
+    }
+
+    @ParameterizedTest 
+    @MethodSource("proverDadosTipoConteudo") 
+    public void tipoConteudoDeveTerNumEDescricaoCorretos(int num_esperado, String description_esperada, TipoConteudo tipo_conteudo) {
+        int num = tipo_conteudo.getNum();
+        String descricao = tipo_conteudo.getDescription();
+
+        assertThat(num).isEqualTo(num_esperado);
+        assertThat(descricao).isEqualTo(description_esperada);        
+    }
+
+    private static Stream<Arguments> proverDadosTipoConteudo() {
+        return Stream.of(
+            Arguments.of(0, "texto", TipoConteudo.TEXTO),
+            Arguments.of(1, "pdf", TipoConteudo.PDF)
+        );
+    }
+
+    @ParameterizedTest 
+    @MethodSource ("proverDadosStatusConteudo")
+    public void statusConteudoDeveTerNumEDescricaoCorretos(int num_esperado, String description_esperada, StatusConteudo status_conteudo) {
+        int num = status_conteudo.getNum();
+        String descricao = status_conteudo.getDescription();
+
+        assertThat(num).isEqualTo(num_esperado);
+        assertThat(descricao).isEqualTo(description_esperada);        
+    }
+
+    private static Stream<Arguments> proverDadosStatusConteudo() {
+        return Stream.of(
+            Arguments.of(0, "revisao", StatusConteudo.REVISAO),
+            Arguments.of(1, "devolvido", StatusConteudo.DEVOLVIDO),
+            Arguments.of(2, "publicado", StatusConteudo.PUBLICADO)
+        );
+    }
+}
---
+++ api/src/test/java/com/dtacomp/schema/classes/ConteudoTextoTest.java
@@ -0,0 +1,20 @@
+package com.dtacomp.schema.classes;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+
+public class ConteudoTextoTest {
+    @Test 
+    public void deveConstruirComBuilder() {
+        UUID idConteudo = UUID.randomUUID();
+        Conteudo conteudo = Conteudo.builder().id(idConteudo).titulo("Teste").build();
+        ConteudoTexto conteudo_pdf = ConteudoTexto.builder().idConteudo(idConteudo).conteudo(conteudo).corpoHtml("Lorem Ipsom Dolor").build();
+
+        assertThat(conteudo_pdf.getIdConteudo()).isEqualTo(idConteudo);
+        assertThat(conteudo_pdf.getConteudo()).isEqualTo(conteudo);
+        assertThat(conteudo_pdf.getCorpoHtml()).isEqualTo("Lorem Ipsom Dolor");
+    }
+}
